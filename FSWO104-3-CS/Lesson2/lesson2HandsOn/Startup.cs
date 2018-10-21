e26a9d339a5c9ef3589a7965437c12b419847957
Brian <brian@tailboardtech.com>
2018-10-21 18:02:09 -0400

    completed lesson 3, added workshop folder, RSVP app added

---
+++ FSWO104-3-CS/Lesson2/lesson2HandsOn/Startup.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace lesson2HandsOn
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+            app.Run(async (context) =>
+            {
+                
+                var cookie = context.Request.Cookies["MyCoolLittleCookie"];
+
+                if (string.IsNullOrWhiteSpace(cookie))
+                {
+                    DateTime now = DateTime.Now;
+                    DateTime expires = now + TimeSpan.FromSeconds(15);
+                    context.Response.Cookies.Append
+                    (
+                        "MyCoolLittleCookie",
+                        "Cookie created at: " + now.ToString("h:mm:ss tt"),
+                        new CookieOptions
+                        {
+                            Path = "/",
+                            HttpOnly = false,
+                            Secure = false,
+                            Expires = expires
+                        }
+                    );
+                }
+
+                string response =
+                    "<h1>HTTP Cookies</h1>" +
+                    $"<p>This is the cookie value received from browser: \"<strong>{cookie}</strong>\".</p>" +
+                    "<p>Refresh page to see current cookie value...</p>" +
+                    "<p>Cookie expires after 15 seconds.</p>";
+                await context.Response.WriteAsync(response);
+            });
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
---
+++ FSWO104-3-CS/PartyRSVP/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using PartyRSVP.Models;
+
+namespace PartyRSVP.Controllers {
+    public class HomeController : Controller {
+        public ViewResult Index() {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse) {
+            // Todo: Store guest response, covered in later lessons
+            return View("Thanks", guestResponse);
+        }
+
+        public IActionResult Rsvp()
+        {
+            return View();
+        }
+
+    }
+}
