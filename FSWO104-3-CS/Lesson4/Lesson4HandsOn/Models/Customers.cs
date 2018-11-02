64e9daf60d20ab746b69f91ee9330737acefbb4c
Brian <brian@tailboardtech.com>
2018-11-02 00:27:20 -0400

    completed lesson 4 handson

---
+++ FSWO104-3-CS/Lesson4/Lesson4HandsOn/Models/Customers.cs
@@ -0,0 +1,18 @@
+namespace Lesson4HandsOn {
+    public class Customers {
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Company { get; set; }
+        public int? Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public int PostalCode { get; set; }
+        public int Phone { get; set; }
+        public int Fax { get; set; }
+        public string Email { get; set; }
+        public int SupportRepId { get; set; }
+
+    }
+}
---
+++ FSWO104-3-CS/Lesson4/Lesson4HandsOn/Models/Employees.cs
@@ -0,0 +1,19 @@
+namespace Lesson4HandsOn {
+    public class Employees {
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Title { get; set; }
+        public string ReportsTo { get; set; }
+        public int? BirthDate { get; set; }
+        public int? HireDate { get; set; }
+        public int? Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int PostalCode { get; set; }
+        public int Phone { get; set; }
+        public int Fax { get; set; }
+        public string Email { get; set; }
+
+    }
+}
